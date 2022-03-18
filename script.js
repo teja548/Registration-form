72ba48407d18299821e133a84cf60e42880178cc
teja548 <tet9692@gmail.com>
2022-03-18 19:46:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const prevBtns = document.querySelectorAll('.btn-prev');
+const nextBtns = document.querySelectorAll('.btn-next');
+const progress = document.getElementById('progress');
+const formSteps = document.querySelectorAll('.form-step');
+const progressSteps = document.querySelectorAll('.progress-step');
+
+const email = document.getElementById('email');
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const password = document.getElementById('password');
+const checkbox = document.getElementById('checkbox');
+const occupation = document.getElementById('occupation');
+const gender = document.getElementById('gender');
+const address = document.getElementById('address');
+const college = document.getElementById('college');
+
+let formStepsNum = 0;
+
+nextBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        formStepsNum++;
+        updateFormSteps();
+        updateProgressbar();
+        const userData = {
+            email: email.value,
+            first: first.value,
+            last: last.value,
+            password: password.value,
+            checkbox: checkbox.value,
+            occupation: occupation.value,
+            gender: occupation.gender,
+            address: occupation.address,
+            college: college.value,
+        };
+
+        localStorage.setItem('data', JSON.stringify(userData));
+    });
+});
+
+prevBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        formStepsNum--;
+        updateFormSteps();
+        updateProgressbar();
+    });
+});
+
+function updateFormSteps() {
+    formSteps.forEach(formStep => {
+        formStep.classList.contains('form-step-active') && formStep.classList.remove('form-step-active');
+    });
+
+    formSteps[formStepsNum].classList.add('form-step-active');
+}
+
+function updateProgressbar() {
+    progressSteps.forEach((progressStep, idx) => {
+        if (idx < formStepsNum + 1) {
+            progressStep.classList.add('progress-step-active');
+        } else {
+            progressStep.classList.remove('progress-step-active');
+        }
+    });
+
+    const progressActive = document.querySelectorAll('.progress-step-active');
+
+    progress.style.width = ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + '%';
+}
